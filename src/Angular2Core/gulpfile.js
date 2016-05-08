6fb31c2c9405b8d4216cae4f434050b1e11c83ae
Jesse Goeglein <gigsguard-shop@yahoo.com>
2016-05-08 17:38:03 -0600

    Automatically copying angular2 dependencies post build.

--- src/Angular2Core/gulpfile.js
+++ src/Angular2Core/gulpfile.js
@@ -1,24 +1,30 @@
 ﻿var gulp = require('gulp');
 
+var paths = {
+    wwwroot: "./wwwroot/"
+};
+paths.libDest = paths.wwwroot + "/lib";
+paths.node_modules = "./node_modules";
+
 gulp.task('copy-angular', function () {
-    return gulp.src('node_modules/@angular/**/*.js').pipe(gulp.dest('wwwroot/lib/@angular'));
+    return gulp.src(paths.node_modules + '/@angular/**/*.js').pipe(gulp.dest(paths.libDest + '/@angular'));
 });
 gulp.task('copy-systemjs', function () {
-    return gulp.src('node_modules/systemjs/**/*.js').pipe(gulp.dest('wwwroot/lib/systemjs'));
+    return gulp.src(paths.node_modules + '/systemjs/**/*.js').pipe(gulp.dest(paths.libDest + '/systemjs'));
 });
 gulp.task('copy-es6-shim', function () {
-    return gulp.src('node_modules/es6-shim/**/*.js').pipe(gulp.dest('wwwroot/lib/es6-shim'));
+    return gulp.src(paths.node_modules + '/es6-shim/**/*.js').pipe(gulp.dest(paths.libDest + '/es6-shim'));
 });
 gulp.task('copy-zone-js', function () {
-    return gulp.src('node_modules/zone.js/**/*.js').pipe(gulp.dest('wwwroot/lib/zone.js'));
+    return gulp.src(paths.node_modules + '/zone.js/**/*.js').pipe(gulp.dest(paths.libDest + '/zone.js'));
 });
 gulp.task('copy-reflect-metadata', function () {
-    return gulp.src('node_modules/reflect-metadata/**/*.js').pipe(gulp.dest('wwwroot/lib/reflect-metadata'));
+    return gulp.src(paths.node_modules + '/reflect-metadata/**/*.js').pipe(gulp.dest(paths.libDest + '/reflect-metadata'));
 });
 gulp.task('copy-rxjs', function () {
-    return gulp.src('node_modules/rxjs/**/*.js').pipe(gulp.dest('wwwroot/lib/rxjs'));
+    return gulp.src(paths.node_modules + '/rxjs/**/*.js').pipe(gulp.dest(paths.libDest + '/rxjs'));
 });
 gulp.task('copy-angular2-in-memory-web-api', function () {
-    return gulp.src('node_modules/angular2-in-memory-web-api/**/*.js').pipe(gulp.dest('wwwroot/lib/angular2-in-memory-web-api'));
+    return gulp.src(paths.node_modules + '/angular2-in-memory-web-api/**/*.js').pipe(gulp.dest(paths.libDest + '/angular2-in-memory-web-api'));
 });
 gulp.task('copy-dependencies', ['copy-angular', 'copy-systemjs', 'copy-es6-shim', 'copy-zone-js', 'copy-reflect-metadata', 'copy-rxjs', 'copy-angular2-in-memory-web-api']);
